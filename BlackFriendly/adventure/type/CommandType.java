3f72e73ac594095c37b14378e86ad406c98a5bb9
Chiara Rigante <c.rigante@studenti.uniba.it>
2020-10-31 12:56:25 +0100

    Ho sistemato la nostra cartella del progetto con alcune modifiche anche nel codice

---
+++ BlackFriendly/adventure/type/CommandType.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BlackFriendly.adventure.type;
+
+/**
+ *
+ * @author Rigante Chiara
+ * @author Ramkalawon Alessia
+ * @author Scalzo Andrea
+ */
+public enum CommandType {
+    END, INVENTORY, NORD, SOUTH, EAST, WEST, OPEN, CLOSE, PUSH, PULL, PICK_UP, GIVE, USE, LOOK_AT, TURN_ON, TURN_OFF,HELP
+}
