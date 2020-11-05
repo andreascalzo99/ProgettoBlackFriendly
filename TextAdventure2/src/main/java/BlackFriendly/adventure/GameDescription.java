cd64b32bd820b6aac823b0ca6b69f1dcd6245e58
Chiara Rigante <c.rigante@studenti.uniba.it>
2020-11-05 19:14:37 +0100

    Questo è il codice corretto

---
+++ TextAdventure2/src/main/java/BlackFriendly/adventure/GameDescription.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BlackFriendly.adventure;
+
+import BlackFriendly.parser.ParserOutput;
+import BlackFriendly.type.AdvObject;
+import BlackFriendly.type.Command;
+import BlackFriendly.type.Room;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Rigante Chiara
+ * @author Ramkalawon Alessia
+ * @author Scalzo Andrea
+ */
+public abstract class GameDescription {
+
+    private final List<Room> rooms = new ArrayList<>();
+
+    private final List<Command> commands = new ArrayList<>();
+
+    private final List<AdvObject> inventory = new ArrayList<>();
+
+    private Room currentRoom;
+
+    public List<Room> getRooms() {
+        return rooms;
+    }
+
+    public List<Command> getCommands() {
+        return commands;
+    }
+
+    public Room getCurrentRoom() {
+        return currentRoom;
+    }
+
+    public void setCurrentRoom(Room currentRoom) {
+        this.currentRoom = currentRoom;
+    }
+
+    public List<AdvObject> getInventory() {
+        return inventory;
+    }
+
+    public abstract void init() throws Exception;
+
+    public abstract void nextMove(ParserOutput p, PrintStream out);
+
+}
